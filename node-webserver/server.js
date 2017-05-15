070d0c24ffcd7a614af8cf255173fd7c65b4947b
Robert Saunders <robertwsaunders@outlook.com>
2017-05-14 21:05:15 -0400

    learned templating with partials and helpers

--- node-webserver/server.js
+++ node-webserver/server.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const hbs = require("hbs");
 
 let app = express();
 
-app.use(express.static(__dirname + '/public'))
+hbs.registerPartials(__dirname + '/views/partials');
 
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
 app.get("/", (req, res) => {
   // res.send('<h1>hello express!</h1>');
   res.send({
     name: "robert"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
   });
 });
 
