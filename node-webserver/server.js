899bbcc4693429c7025e1b083be15af0b5bb4866
Robert Saunders <robertwsaunders@outlook.com>
2017-05-16 10:11:46 -0400

    added a maintenance page and learned about middleware

--- node-webserver/server.js
+++ node-webserver/server.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const hbs = require("hbs");
-
+const fs = require("fs");
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
+
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to the server.log!');
+    }
+  });
+  next();
+});
+
+
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
