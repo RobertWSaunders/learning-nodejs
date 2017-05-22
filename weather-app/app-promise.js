2a1ca018e12fa2500e417eaf05dfebcdc5ef4dbc
Robert Saunders <robertwsaunders@outlook.com>
2017-05-22 11:08:17 -0400

    Finished weather app

--- weather-app/app-promise.js
+++ weather-app/app-promise.js
@@ -9,3 +9,29 @@ const argv = yargs.options({
     string: true
   }
 }).help().alias('help', 'h').argv;
+
+let encodedAddress = encodeURIComponent(argv.a);
+let geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+
+axios.get(geocodeURL).then((response) => {
+  if (response.data.status === 'ZERO_RESULTS') {
+    throw new Error('Unable to find address!');
+  }
+  const API_KEY = '8233ef70106ff6b43dfa5db010ddbad5';
+  let lat = response.data.results[0].geometry.location.lat;
+  let long = response.data.results[0].geometry.location.lng;
+  let weatherURL = `https://api.darksky.net/forecast/${API_KEY}/${lat},${long}`;
+  console.log(response.data.results[0].formatted_address);
+  return axios.get(weatherURL);
+}).then((response) => {
+  var temp = response.data.currently.temperature;
+  var atemp = response.data.currently.apparentTemperature;
+  console.log(`It's currently ${atemp}`);
+}).catch((e) => {
+  if (e.code === 'ENOTFOUND') {
+    console.log('Unable to connect to API Servers!');
+  } else {
+    console.log(e.message);
+  }
+});
